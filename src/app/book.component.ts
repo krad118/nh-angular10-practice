cfda5b10b728332be9ddd072b5e4a4e312756e04
Jose Ramirez <jcramireztello@gmail.com>
2020-12-02 14:47:44 -0500

    feat: learning about life cycle Angular
    closed #5

--- src/app/book.component.ts
+++ src/app/book.component.ts
@@ -1,11 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styles: []
 })
-export class BookComponent {
+export class BookComponent implements OnChanges, OnInit, OnDestroy {
   @Input() book;
 
+  constructor() {
+    console.log(`life cycle Angular: contructor`);
+  }
+
+  ngOnChanges(change: SimpleChanges): void {
+    console.log(`life cycle Angular: ngOnChanges ${change}`);
+  }
+
+  ngOnInit(): void {
+    console.log(`life cycle Angular: ngOnInit`);
+  }
+
+  ngOnDestroy(): void {
+    console.log(`life cycle Angular: ngOnDestroy`);
+  }
 }
